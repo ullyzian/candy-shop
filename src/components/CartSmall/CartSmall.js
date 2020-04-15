888eab5cb54ac3571ac722dfd45860b6ed8b528b
tekkenthuuug <sa.soto317@gmail.com>
2020-04-15 21:55:17 +0200

    Fix cart menu state

--- src/components/CartSmall/CartSmall.js
+++ src/components/CartSmall/CartSmall.js
@@ -8,9 +8,9 @@ import { ReactComponent as CartLogo } from "../../img/cart.svg";
 import "./CartSmall.scss";
 
 const CartSmall = () => {
-  const [showMenu, setShowMenu] = useState(true);
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <div className="cart-small" onMouseEnter={() => setShowMenu(true)} onMouseLeave={() => setShowMenu(true)}>
+    <div className="cart-small" onMouseEnter={() => setShowMenu(true)} onMouseLeave={() => setShowMenu(false)}>
       <CartLogo />
       <div className="cart-small__counter-wrapper">
         <CartCounter />
